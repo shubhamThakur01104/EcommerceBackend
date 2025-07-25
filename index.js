07a499a8cd93ff7d64be8a6fc9660b73404f6721
shubhamThakur01104 <sonujamal01@gmail.com>
2025-07-25 16:27:13 +0530

    Add Live

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
-app.use(cors({ origin: "http://localhost:3000" }))
+app.use(cors({ origin: ["https://ecommerce-frontend-228h.vercel.app/","https://ecommerce-frontend-228h.vercel.app"] }))
 
 const startServer = async () => {
     try {
